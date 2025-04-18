f06e4e7992bfbec53c0e17dd9f9c18f974474329
prudhvir3ddy <>
2025-04-18 11:27:43 +0530

    bug: using multiple registries doesn't work
    As per this guide, multiple registries should work
    
    https://googlesamples.github.io/android-custom-lint-rules/api-guide.html
    Signed-off-by: prudhvir3ddy <>

--- app/src/main/java/com/android/example/Test.kt
+++ app/src/main/java/com/android/example/Test.kt
@@ -1,10 +1,14 @@
 package com.android.example
 
+import java.util.Date
+
 class Test {
     // We have a custom lint check bundled with :library
     // that this module depends on. The lint check looks
     // for mentions of "lint", which should trigger an
     // error
     val s = "lint"
-    fun lint() { }
+    fun lint() {
+      val date = Date()
+    }
 }
